82ee2a3a59e68467fffd37303d9cb4447588c60a
Jose <jose.rivera@utp.edu.co>
2021-12-09 17:49:27 -0500

    first p

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { withLatestFrom } from 'rxjs';
+import { Usuario } from './model/usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Proyecto';
+  public nombres: string[] = ['JD','NJ'];
+
+  public nombres2: string[] = ['DJ'];
+
+  public listusuario: Usuario[] = [];
+
+  constructor(){
+    this.addFirstElement('A');
+    this.addLastElement('B')
+    this.deleteElement();
+    this.nombres = this.concat(this.nombres2)
+    console.log(this.nombres);
+
+    console.log(this.nombres.sort());
+    console.log(this.nombres.reverse());
+
+    console.log(this.nombres.splice(this.nombres.indexOf("DJ"),1))
+
+    let usuario: Usuario = new Usuario();
+    usuario.idUsuario = 1;
+    usuario.edad = 20;
+    usuario.nombre = "Ricardo";
+    usuario.estado = true;
+
+    let usuario2: Usuario = new Usuario();
+    usuario2.idUsuario = 1;
+    usuario2.edad = 20;
+    usuario2.nombre = "Jose";
+    usuario2.estado = true;
+
+    this.listusuario.push(usuario2);
+
+  }
+
+public addFirstElement(nombre: string){
+  this.nombres.unshift(nombre);
+}
+
+public addLastElement(nombre: string): void{
+  this.nombres.push(nombre);
+}
+
+public concat(list:string[]): string[]{
+  return this.nombres.concat(list)
+}
+
+public deleteElement(): void{
+  let res=this.nombres.pop();
+  console.log("se elimino el registro " + res);
+}
+
 }
---
+++ src/app/model/usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario{
+    public idUsuario: number = 0;
+    public nombre: string = '';
+    public edad: number = 0;
+    public estado: boolean= false;
+
+}
